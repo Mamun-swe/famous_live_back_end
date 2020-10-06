aa0f8a556306469e42a2dd05c316905bf7bd410f
Abdullah Al Mamun <mhmamun166009@gmail.com>
2020-10-06 17:49:53 +0600

    okk

--- api/controllers/User/AllUsers.js
+++ api/controllers/User/AllUsers.js
@@ -2,58 +2,53 @@ const jwt = require('jsonwebtoken')
 const Users = require('../../../models/users')
 const rootURl = require('../../../utils/rootURL')
 
+
 const getAllUsers = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1]
         const decode = jwt.verify(token, 'SECRET')
-        let id = decode.id
 
-        const users = await Users.find(
-            { _id: { $nin: [id] }, role: { $nin: ['admin'] } },
-            { access_token: 0 }
-        )
-            .populate({ path: 'following', select: 'name phone image' })
-            .populate({ path: 'followers', select: 'name phone image' })
-            .exec()
+        const result = await Users.findOne({ _id: decode.id }, { block_users: 1, _id: 0 })
+        const myBlockedUsers = result.block_users
 
-        if (!users) {
+        const allUsers = await Users.find(
+            {
+                $and: [
+                    { "_id": { $nin: myBlockedUsers } },
+                    { "_id": { $nin: decode.id } },
+                    { "role": { $nin: ['admin'] } }
+                ]
+            },
+            {
+                name: 1,
+                isLive: 1,
+                status: 1,
+                userLevel: 1,
+                image: 1
+            }
+        ).exec()
+
+        if (!allUsers) {
             return res.status(204).json('Users not found')
         }
 
         const response = {
-            results: users.map(user => {
+            results: allUsers.map(user => {
                 return {
                     id: user._id,
                     name: user.name,
-                    phone: user.phone,
-                    role: user.role,
                     status: user.status,
                     isLive: user.isLive,
-                    mainCoinBalane: user.mainCoinBalane,
-                    presentCoinBalance: user.presentCoinBalance,
                     userLevel: user.userLevel,
                     image: rootURl + "uploads/images/" + user.image,
-                    followers: user.followers.map(follower => {
-                        return {
-                            id: follower._id,
-                            name: follower.name,
-                            phone: follower.phone,
-                            image: rootURl + "uploads/images/" + follower.image
-                        }
-                    }),
-                    following: user.following.map(follow => {
-                        return {
-                            id: follow._id,
-                            name: follow.name,
-                            phone: follow.phone,
-                            image: rootURl + "uploads/images/" + follow.image
-                        }
-                    })
                 }
             })
         }
         res.status(200).json(response)
+
+
     } catch (error) {
+        console.log(error.message)
         next(error)
     }
 }
